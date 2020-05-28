83254524ba222eeb5b63a0420358a2c3e07a6dcd
Xiao yi Hu <xy1998@live.nl>
2020-05-28 16:40:13 +0200

    project now compiles on windows using vs 2019

---
+++ RayEngine/RayEngine_VS_CODE/ObjectStorage_2D.cpp
@@ -0,0 +1,32 @@
+#include "ObjectStorage_2D.hpp"
+
+ObjectStorage_2D::ObjectStorage_2D(){}
+
+std::vector<Light_2D>& ObjectStorage_2D::getLightVector(){
+    return lightVector;
+}
+
+std::vector<std::shared_ptr<Circle_2D>>& ObjectStorage_2D::getobjects(){
+    return objects;
+}
+
+void ObjectStorage_2D::init(){
+    lightVector = {
+            // Light_2D(sf::Vector2f(400, 375), sf::Vector3f(0.2, 0.4, 0.7), 5),
+            Light_2D(sf::Vector2f(200, 250), sf::Vector3f(0.7, 0.4, 0.3), 5),
+            Light_2D(sf::Vector2f(-50, 150), sf::Vector3f(0.1, 0.2, 0.1), 5),
+            // Light_2D(sf::Vector2f(0, 50), sf::Vector3f(0.2, 0.5, 0.4), 5)
+    };
+
+    objects = std::vector<std::shared_ptr<Circle_2D>>{
+            std::make_shared<Circle_2D>(sf::Vector2f(350,250), 20, 50),
+            std::make_shared<Circle_2D>(sf::Vector2f(120,340), 20, 64),
+            std::make_shared<Circle_2D>(sf::Vector2f(500,50), 30, 21),
+            std::make_shared<Circle_2D>(sf::Vector2f(150,100), 50, 15),
+
+            std::make_shared<Circle_2D>(sf::Vector2f(240,170), 6),
+            std::make_shared<Circle_2D>(sf::Vector2f(20,247), 12),
+            std::make_shared<Circle_2D>(sf::Vector2f(431,206), 24),
+            std::make_shared<Circle_2D>(sf::Vector2f(253,302), 10)
+    };
+}
